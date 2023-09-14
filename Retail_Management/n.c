8b8850a3f2cbc7296962f59b7047de8109c164b5
Tushar Kanti Saha <87900065+Tushar58a@users.noreply.github.com>
2023-09-14 22:21:30 +0530

    Add files via upload

---
+++ Retail_Management/n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i=2,j=0,k=-2,l=5,m=0;
+    
+    m=i++&&++j&&++k|| l++;
+
+ printf ("%d %d %d %d %d\n",i,j,k,l,m);
+
+    
+    
+
+
+}
---
+++ Retail_Management/n.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+    char str1[]="hello";
+    char str2[5]="hello";
+    printf("96lu 96lu",sizeof(str1),sizeof(str2));
+
+}
